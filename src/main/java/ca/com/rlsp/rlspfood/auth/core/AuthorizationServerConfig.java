ba5e6ea958cbb7b53af27459a269d3e8d2d3a443
Rodrigo Latorraca <rlatorraca@gmail.com>
2021-11-19 13:09:31 -0400

    Fix JpaUSerDetailsService class

--- src/main/java/ca/com/rlsp/rlspfood/auth/core/AuthorizationServerConfig.java
+++ src/main/java/ca/com/rlsp/rlspfood/auth/core/AuthorizationServerConfig.java
@@ -52,9 +52,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private RedisConnectionFactory redisConnectionFactory;
      */
 
-
-
-
     //@formatter:off
 
     /**
